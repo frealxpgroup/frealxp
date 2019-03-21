b40c6aaa64427d31963ba60dd4092a25d8b7c2e4
Tyson Hancock <tyson@Tysons-MacBook-Pro.local>
2019-03-21 10:21:31 -0600

    made changes to the routes file

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Switch, Route} from 'react-router-dom'
+
+export default (
+    <Switch>
+        <Route exact path='/' component={} />
+        <Route path='/' component={} />
+        <Route path='/' component={} />
+        <Route path='/' component={} />
+        <Route path='/' component={} />
+        <Route path='/' component={} />
+        <Route path='/' component={} />
+        <Route path='/' component={} />
+        <Route path='/' component={} />
+        <Route path='/' component={} />
+        <Route path='/' component={} />
+        <Route path='/' component={} />
+    </Switch>
+)
