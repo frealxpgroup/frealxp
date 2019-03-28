0e7afe4da358bd2d2188510aec83dfe8410df10c
Tyson Hancock <tyson@Tysons-MacBook-Pro.local>
2019-03-28 08:47:48 -0600

    minor changes

--- src/Views/History/History.js
+++ src/Views/History/History.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Bar } from 'react-chartjs-2';
+import { LineChart } from 'react-chartjs-2';
+// var LineChart = require("react-chartjs").Line;
 
-const History = () => {
-    return (
-        <div>History Page</div>
-    )
-}
+class History extends Component {
+
+    render() {
+        return (
+            <div> History
+                {/* <LineChart data={chartData} options={chartOptions} />
+                <Bar
+                    data={data}
+                    width={100}
+                    height={50}
+                    options={{ maintainAspectRatio: false }}
+                /> */}
+            </div> 
+        )
+    }
+};
 export default History
