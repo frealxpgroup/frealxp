fcd70752e841f184324c0045b97fdbf03007a021
Tyson Hancock <tyson@Tysons-MacBook-Pro.local>
2019-04-01 11:35:53 -0600

    still working on the same function

--- src/Views/History/LineGraph.js
+++ src/Views/History/LineGraph.js
@@ -47,6 +47,51 @@ class LineGraph extends Component {
 
     render() {
         console.log(this.state.user_id)
+//         var date_test = new Date("2011-07-14 11:23:00".replace(/-/g,"/"));
+// console.log(date_test);
+        const mappedDates = this.state.Dates.map((date, i) => {
+            let myDate = new Date(date.approved_date)
+            let stringDate = myDate.toString()
+
+            let newJan = 0;
+            let newFeb = 0;
+            let newMar = 0;
+            if (stringDate.includes('Jan')){
+                return newJan =+ 1
+            }
+            else if (stringDate.includes('Feb')){
+                return newFeb =+ 1
+            }
+            else if (stringDate.includes('Mar')){
+                return newMar =+ 1
+            }
+            this.setState({
+                Jan: newJan,
+                Feb: newFeb,
+                Mar: newMar
+            })
+
+           
+            // console.log(myDate)
+            console.log( typeof stringDate)
+            console.log( newJan)
+            console.log( newFeb)
+            console.log( newMar)
+            
+            // console.log(dateChanger)
+            return (
+                <div>
+                   <h2>{date.approved_date}</h2> 
+                    <h3>{console.log( typeof stringDate)}</h3>
+                    <h4>{console.log( newJan)}</h4>
+                    <p>{console.log( newFeb)}</p>
+                    <p>{console.log( newMar)}</p>
+
+
+                </div>
+                
+            )
+        })
 
         const data = {
             labels: [
@@ -124,7 +169,8 @@ class LineGraph extends Component {
             <div>
             <button onClick={this.getIdFromTracked}>PUSH ME PLS</button>
             <Line data={data} options={options} />
-            <h1>{this.state.Dates}</h1>
+            <h1>{mappedDates}</h1>
+            <h2>{console.log(mappedDates)}</h2>
             </div>
         )
     }
