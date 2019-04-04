f3a4c99ddce6d87673cf5fd6879578767bd81259
Dallin-H <dallinhyde@gmail.com>
2019-04-03 22:53:27 -0600

    challenge review front end

--- src/Views/Challenge/review.js
+++ src/Views/Challenge/review.js
@@ -10,22 +10,23 @@ class Review extends Component {
     return (
       <div className="review_background">
         <div className="review_nav">
-            <div className='review_logo'>FrealXP</div>
+          <div className="review_logo">FrealXP</div>
         </div>
         <div className="review_body_content">
-          Main box
-          <div className="review_body_content_row1">
-            <div className="review_challenge_title">title</div>
-            <div className="review_date">date</div>
-          </div>
-          <div className="review_body_content_row2">
-            <div className="review_description">description</div>
-            <div className="review_image">image</div>
-          </div>
-          <div className="review_feedback">feedback</div>
-          <div className="review_body_content_row4">
-            <div className="review_button">SEND BACK</div>
-            <div className="review_button">APPROVE</div>
+          <div className="review_body_content_inner">
+            <div className="review_body_content_row1">
+              <div className="review_challenge_title">title</div>
+              <div className="review_date">date</div>
+            </div>
+            <div className="review_body_content_row2">
+              <div className="review_description">description</div>
+              <div className="review_image">image</div>
+            </div>
+            <div className="review_feedback">feedback</div>
+            <div className="review_body_content_row4">
+              <div className="review_button">SEND BACK</div>
+              <div className="review_button">APPROVE</div>
+            </div>
           </div>
         </div>
       </div>
